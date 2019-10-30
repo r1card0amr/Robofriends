e93869bf4a1f3f649a3d07fd97dd637ae1d54d3d
Ricardo Amr <ricardo.daniel.amr@gmail.com>
2019-10-30 23:24:51 +0000

    Updates

---
+++ precache-manifest.2a7c5eac24e97f053881a985ae760079.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9960b7d9a5919d1f0ecd24ec4fc7e0e6",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "db030a0313823e763c87",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "df9ac06e92e949f2848d",
+    "url": "/robofriends/static/css/main.0ff520dc.chunk.css"
+  },
+  {
+    "revision": "db030a0313823e763c87",
+    "url": "/robofriends/static/js/2.fbad7c07.chunk.js"
+  },
+  {
+    "revision": "df9ac06e92e949f2848d",
+    "url": "/robofriends/static/js/main.32c0c276.chunk.js"
+  },
+  {
+    "revision": "15646e7ae3f7e7d4e926",
+    "url": "/robofriends/static/js/runtime-main.59eca5ac.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
